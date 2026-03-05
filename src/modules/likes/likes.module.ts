072514e2f6e9e098728afe793b2e164b7442acc8
panyaphat insawng <68011743@kmitl.ac.th>
2026-03-06 02:20:58 +0700

    add likes.controller

--- src/modules/likes/likes.module.ts
+++ src/modules/likes/likes.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { LikesService } from './likes.service';
-
+import { LikesController } from './likes.controller'
 /**
  * Module สำหรับ likes
  */
 
 @Module({
+  controllers:[LikesController],
   providers: [LikesService],
   exports: [LikesService],
 })
